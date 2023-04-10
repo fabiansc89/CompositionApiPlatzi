278c4d26b9a0912c579650cdecd33c33ee764feb
fasotonc <fabiansc89@gmail.com>
2023-04-10 17:22:17 -0400

    curso compositionapi

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <Home first-name="Fabian Gabriel" last-name="Soto Canales"></Home>
+</template>
+
+<script>
+import Home from "@/components/Home.vue";
+import { provide } from "vue";
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+  setup() {
+    provide("username", "Fabito");
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>{{ fullName }}</div>
+    <div>{{ username }}</div>
+    <button ref="btn">Click!</button>
+  </template>
+  
+  <script setup>
+  import { defineProps, defineExpose, ref, toRefs, computed, watch, inject } from "vue";
+  const props = defineProps({
+    firstName: String,
+    lastName: String,
+  });
+  const { firstName, lastName } = toRefs(props);
+  const fullName = computed(() => {
+    return `${firstName.value} ${lastName.value}`;
+  });
+  const username = inject("username");
+  defineExpose({
+    fullName,
+  });
+  const btn = ref(null);
+  console.log(btn.value);
+  watch(btn, (valor) => {
+    console.log(valor);
+  });
+  </script>
